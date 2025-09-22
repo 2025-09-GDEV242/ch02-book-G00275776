2ca6c9d3a653aa6f8ef741aeffa2d543c0813e44
G00275776 <g00275776@stu.raritanval.edu>
2025-09-22 17:40:57 -0400

    Add files via upload

--- Book.java
+++ Book.java
@@ -3,24 +3,51 @@
  * This might form part of a larger application such
  * as a library system, for instance.
  *
- * @author (Insert your name here.)
- * @version (Insert today's date here.)
+ * @author Jimmy Arias
+ * @version 9/22/25
  */
 class Book
 {
     // The fields.
     private String author;
     private String title;
+    private int pages;
 
     /**
      * Set the author and title fields when this object
      * is constructed.
      */
-    public Book(String bookAuthor, String bookTitle)
+    public Book(String bookAuthor, String bookTitle, int bookPages)
     {
         author = bookAuthor;
         title = bookTitle;
+        pages = bookPages;
     }
 
-    // Add the methods here ...
+    
+    private String getAuthor ()
+    {
+        return author;
+    }
+    
+    private String getTitle()
+    {
+        return title;
+    }
+    
+    private int getPages()
+    {
+        return pages;
+    }
+    
+    private void printAuthor()
+    {
+        System.out.println(author);
+    }
+    
+    private void printTitle()
+    {
+        System.out.println(title);
+    }
+    
 }
